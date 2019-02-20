66da0eb0d78486f12671ebac18ed029077bdcf69
Ella Ge <eirage@chromium.org>
2019-02-20 13:21:26 -0500

    update

--- FrameCapture/Capture.js
+++ FrameCapture/Capture.js
@@ -2,16 +2,14 @@ let PEList = ["pointerenter", "pointerleave", "pointerover", "pointerout", "poin
       "pointerup", "gotpointercapture", "lostpointercapture"];
 PEList.forEach(function(eventName) {
   document.addEventListener(eventName, function(e) {
-    console.log(document.title + " " + e.type + " " + e.target.nodeName);
+    console.log(document.title + " " + e.type + " " + (e.target.id ? e.target.id : e.target.nodeName));
   });
 });
-
 function SetCapture(event) {
   event.target.setPointerCapture(event.pointerId);
 }
 
 function PreventDefault(event) {
-    console.log("hello")
     event.preventDefault();
 }
 
