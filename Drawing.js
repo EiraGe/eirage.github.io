7058d0f839c3175ca5c6ebfc2780f3d41d2d792b
Ella Ge <eirage@chromium.org>
2019-07-10 15:18:55 -0400

    update

---
+++ Drawing.js
@@ -0,0 +1,139 @@
+var points = [];
+var predictedPoints = [];
+var coalescedPoints = [];
+var startTime;
+var canvas;
+var colorCounter = {};
+const colors = ["rgba(255, 128, 0, ", "rgba(255, 255, 0, ", "rgba(128, 255, 0, ",
+                "rgba(0, 255, 0, ", "rgba(0, 255, 128, ", "rgba(0, 255, 255, ", " rgba(0, 128, 255, ",
+                "rgba(0, 0, 255, ", "rgba(128, 0, 255, ", "rgba(255, 0, 255, ", "rgba(255, 0, 128, ", "rgba(255, 0, 0, ", ]
+
+function GetContext() {
+  return document.getElementById("canvas").getContext("2d"); 
+}   
+
+window.addEventListener('resize', function(e) {
+    InitializeCanvas();
+});
+
+function drawPoints(points, isCoalesced, isPredicted) {
+  var context = canvas.getContext('2d');
+  for (var i = 0; i < points.length; ++i) {
+    var radius;
+    if (isPredicted) {
+      radius= 5.0
+      context.strokeStyle = colors[points[i].color] + "0.3)";
+    } else if (isCoalesced) {
+      radius = 2.0;
+      context.fillStyle = colors[points[i].color] + "0.9)";
+    } else {
+      radius = 3.0
+      context.strokeStyle = colors[points[i].color] + "0.6)";
+    }
+
+    context.lineWidth = 2
+    context.beginPath();
+
+    context.arc(points[i].x * scale, points[i].y * scale, radius * scale, 0, 2 * 3.14159, false);
+    if (isCoalesced)
+      context.fill();
+    else
+      context.stroke();
+    context.closePath();
+  }
+}
+
+function onFrame()
+{ 
+  if (startTime) {
+    drawPoints(points, false, false);
+    drawPoints(predictedPoints, false, true);
+    drawPoints(coalescedPoints, true, false);
+    points = [];
+    predictedPoints = [];
+    coalescedPoints = [];
+
+    window.requestAnimationFrame(onFrame);
+  }
+}
+
+function startDraw() {
+  startTime = performance.now();
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  window.requestAnimationFrame(onFrame);
+}
+
+function endDraw() {
+  startTime = undefined;
+}
+
+function addPredictedPoint(x, y, color) {
+  predictedPoints.push({x:x, y:y, color:color});
+}
+
+function addCoalescedPoint(x, y, color) {
+  coalescedPoints.push({x:x, y:y, color:color});
+}
+
+function addPoint(x, y, color) {
+  points.push({x:x, y:y, color:color});
+}
+
+function getColorCounter(pointerId) {
+  if (!(pointerId in colorCounter))
+    colorCounter[pointerId] = 0;
+  colorCounter[pointerId] = (colorCounter[pointerId] + 2)  % 12;
+  return colorCounter[pointerId];
+}
+
+window.onload = function() {
+  canvas = document.getElementById('canvas');
+  if (window.PointerEvent) {
+    startDraw();
+    canvas.addEventListener('pointerdown', function(e) {
+      color_counter = getColorCounter(e.pointerId);
+      addPoint(e.pageX, e.pageY, color_counter);
+      e.preventDefault();
+    });
+    canvas.addEventListener('pointerrawupdate', function(e) {
+      color_counter = getColorCounter(e.pointerId);
+      if (e.getPredictedEvents) {
+        e.getPredictedEvents().forEach(function(ce) {
+          addPredictedPoint(ce.pageX, ce.pageY, color_counter);
+        });
+      }
+      if (e.getCoalescedEvents) {
+        e.getCoalescedEvents().forEach(function(ce) {
+          addCoalescedPoint(ce.pageX, ce.pageY, color_counter);
+        });
+      }
+      addPoint(e.pageX, e.pageY, color_counter);
+      e.preventDefault();
+    });
+    canvas.addEventListener('pointerup', function(e) {
+      e.preventDefault();
+    });
+    canvas.addEventListener('contextmenu', function(e) {
+      e.preventDefault();
+      Clear();
+    })
+  }
+  InitializeCanvas();
+}
+
+var scale = 1;
+
+function InitializeCanvas() {
+  var elem = document.getElementById('canvas');
+  var container = document.getElementById('container');
+  
+  scale = window.devicePixelRatio ? window.devicePixelRatio : 1;
+  elem.width = container.clientWidth * scale;
+  elem.height = container.clientHeight * scale;
+}
+
+function Clear() {
+  endDraw();
+  startDraw();
+  colorCounter = {}
+}
