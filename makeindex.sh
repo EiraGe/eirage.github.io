88307608b6b700854b71bdf50417d50f531e2003
Ella Ge <eirage@chromium.org>
2019-10-09 14:57:11 -0400

    update

--- makeindex.sh
+++ makeindex.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-find . -name '*.html' | \
-  perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+find . -name '*.html' | grep -v "bug" | grep -v "index" | \
+  perl -e 'print "<html><body><ul>\n"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>\n";} print "</ul></body></html>"' > index.html
+
+find ./bug/ -name '*.html' | grep -v "index" | \
+  perl -e 'print "<html><body><ul>\n"; while(<>) { $name = substr($_, 6, -6); print "<li><a href=\"./$name.html\">$name</a> <a href=\"https://crbug.com/$name\">(crbug)</a></li>\n";} print "</ul></body></html>"' > bug/index.html
