871073052998b13e2156184c309a1e848df67a14
Ella Ge <eirage@chromium.org>
2020-01-06 13:56:31 -0500

    update

--- makeindex.sh
+++ makeindex.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 find . -name '*.html' | grep -v "bug" | grep -v "index" | \
-  perl -e 'print "<html>\n<link rel=\"manifest\" href=\"./manifest.json\">\n<link rel=\"icon\" href=\"./image/icon-192x192.png\">\n";
+  perl -e 'print "<html>\n<link rel=\"manifest\" href=\"./manifest.json\">\n<link rel=\"icon\" href=\"./images/icon-192x192.png\">\n";
            print "<body><ul>\n"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>\n";} print "<li><a href=\"./bug/index.html\">BUG</a></li>\n"; print "</ul></body></html>"' > index.html
 
 find ./bug/ -name '*.html' | grep -v "index" | \
