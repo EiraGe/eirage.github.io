83bdbdcbd4b2d2baadef1be46081c820f2230b1b
Ella Ge <eirage@chromium.org>
2020-09-09 18:03:00 -0400

    Update

--- update.sh
+++ update.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 ./makeindex.sh
-git add .
 read -p "Commit message [Update]:" message
 message=${message:-Update}
 echo $message
-git commit -am "$message"
-git push
 
 cd PWA
 git add .
 git commit -am "$message"
-git push
 cd ..
 
 cd script
 git add .
 git commit -am "$message"
-git push
 cd ..
+
+git add .
+git commit -am "$message"
+git push
