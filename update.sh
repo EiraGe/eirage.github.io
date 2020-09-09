dd6bd8f937ce05e95dd3701ff0880a1ed5a5b9a6
Ella Ge <eirage@chromium.org>
2020-09-09 17:46:01 -0400

    close

--- update.sh
+++ update.sh
@@ -7,3 +7,15 @@ message=${message:-Update}
 echo $message
 git commit -am "$message"
 git push
+
+cd PWA
+git add .
+git commit -am "$message"
+git push
+cd ..
+
+cd script
+git add .
+git commit -am "$message"
+git push
+cd ..
