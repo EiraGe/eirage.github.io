e265388c639706001298fb18e135114d527fbc74
Ella Ge <eirage@chromium.org>
2020-09-09 17:35:19 -0400

    Make PWA/ submodule

--- update.sh
+++ update.sh
@@ -7,15 +7,3 @@ message=${message:-Update}
 echo $message
 git commit -am "$message"
 git push
-
-cd PWA
-git add .
-git commit -am "$message"
-git push
-cd ..
-
-cd script
-git add .
-git commit -am "$message"
-git push
-cd ..
