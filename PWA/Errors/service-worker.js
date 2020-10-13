3c2b4b123391a50083772f34503192efee473a5c
Ella Ge <eirage@chromium.org>
2020-10-13 11:41:31 -0400

    no modules

---
+++ PWA/Errors/service-worker.js
@@ -0,0 +1,42 @@
+const FILES_TO_CACHE = [
+  './index.html',
+  './blank.html',
+  './link.html'
+];
+
+CACHE_NAME = 'v1';
+
+self.addEventListener('install', (evt) => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+          console.log('[ServiceWorker] Pre-caching offline page');
+          return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (evt) => {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+          return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    );
+});
