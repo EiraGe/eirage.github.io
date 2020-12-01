5fd1eff1594292029ae28e2961a79e9d41f59453
Ella Ge <eirage@chromium.org>
2020-12-01 11:01:48 -0500

    separate file with fix

--- PWA/WebID/gLogin.js
+++ PWA/WebID/gLogin.js
@@ -2,12 +2,12 @@ function ginit() {
   gapi.load('auth2', function() {
     /* Ready. Make a call to gapi.auth2.init or some other API */
     config = {
-      client_id: '756916960051-7ajkfavuf3j9mk3s2eflg02kifa1t6ih.apps.googleusercontent.com'
-      ux_mode: 'redirect'
+      client_id: '756916960051-7ajkfavuf3j9mk3s2eflg02kifa1t6ih.apps.googleusercontent.com',
+      ux_mode: 'redirect',
+      redirect_uri: "https://tests.eirage.com/PWA/WebID/index.html"
     }
   });
 }
-
 
 function onSignIn(googleUser) {
   document.logIn = 'google'
