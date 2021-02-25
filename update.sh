e8f67920df5c300dcf2b3cdc6c629490078b667e
Ella Ge <eirage@chromium.org>
2021-02-25 14:15:54 -0500

    remove PWA/

--- update.sh
+++ update.sh
@@ -5,18 +5,6 @@ read -p "Commit message [Update]:" message
 message=${message:-Update}
 echo $message
 
-cd script
-git add .
-git commit -am "$message"
-git push
-cd ..
-
-cd PWA
-git add .
-git commit -am "$message"
-git push
-cd ..
-
 git add .
 git commit -am "$message"
 git push
