f6ad3de03ad1682616b439e73f936720fe2b5d05
Ella Ge <eirage@chromium.org>
2021-03-15 14:38:05 -0400

    makeindex

--- makeindex.sh
+++ makeindex.sh
@@ -3,5 +3,10 @@
 find . -name '*.html' | grep -v "bug" | \
   perl -e 'print "<html>\n<body><ul>\n"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>\n";} print "<li><a href=\"./bug/index.html\">BUG</a></li>\n"; print "</ul></body>\n</html>"' > index.html
 
+
+find ./PWA/ -name '*.html' | \
+  perl -e 'print "<html><body><ul>\n"; while(<>) { $name = substr($_, 6, -1); print "<li><a href=\"https\://pwa.eirage.com/$name\">$name</a>\n"; } print "</ul></body></html>"' > PWA/index.html
+
+
 find ./bug/ -name '*.html' | grep -v "index" | \
   perl -e 'print "<html><body><ul>\n"; while(<>) { $name = substr($_, 6, -6); print "<li><a href=\"./$name.html\">$name</a> <a href=\"https://crbug.com/$name\">(crbug)</a></li>\n";} print "</ul></body></html>"' > bug/index.html
