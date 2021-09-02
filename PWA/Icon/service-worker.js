05006329d2da2dd06a9f8315b307202da816f3c2
Ella Ge <eirage@chromium.org>
2021-09-02 12:07:40 -0400

    tmp

--- PWA/Icon/service-worker.js
+++ PWA/Icon/service-worker.js
@@ -1,6 +1,5 @@
 const FILES_TO_CACHE = [
   './index.html',
-  './p2.html',
 ];
 
 CACHE_NAME = 'v1';
@@ -42,31 +41,3 @@ self.addEventListener('fetch', (evt) => {
           })
   );
 })
-
-self.addEventListener('notificationclick', function(event) {
-  var notification = event.notification;
-
-  if (!notification.data.hasOwnProperty('options'))
-    return;
-
-  var options = notification.data.options;
-
-  if (options.close)
-    event.notification.close();
-
-  var promise = Promise.resolve();
-
-  promise =
-      promise.then(function() { return firstWindowClient(); })
-             .then(function(client) { return client.focus(); });
-  promise = promise.catch(function() { clients.openWindow(options.url); });
-
-  event.waitUntil(promise);
-});
-
-
-function firstWindowClient() {
-  return clients.matchAll({ type: 'window' }).then(function(windowClients) {
-    return windowClients.length ? windowClients[0] : Promise.reject("No clients");
-  });
-}
