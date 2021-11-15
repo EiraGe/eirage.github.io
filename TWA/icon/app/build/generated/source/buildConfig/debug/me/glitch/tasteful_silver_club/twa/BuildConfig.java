e60b87982065055c998cb201891ae3b3522bc208
Ella Ge <eirage@chromium.org>
2021-11-15 11:30:49 -0500

    Update

---
+++ TWA/icon/app/build/generated/source/buildConfig/debug/me/glitch/tasteful_silver_club/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package me.glitch.tasteful_silver_club.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "me.glitch.tasteful_silver_club.twa";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
---
+++ TWA/icon/app/src/main/java/me/glitch/tasteful_silver_club/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package me.glitch.tasteful_silver_club.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
