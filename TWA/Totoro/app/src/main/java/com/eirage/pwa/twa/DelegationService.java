bc6848ca472f03d8dd70be6be85f8cfc4545646f
Ella Ge <eirage@chromium.org>
2022-08-10 10:20:03 -0700

    Update

--- TWA/Totoro/app/src/main/java/com/eirage/pwa/twa/DelegationService.java
+++ TWA/Totoro/app/src/main/java/com/eirage/pwa/twa/DelegationService.java
@@ -1,6 +1,8 @@
 package com.eirage.pwa.twa;
 
 
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
 
 public class DelegationService extends
         com.google.androidbrowserhelper.trusted.DelegationService {
@@ -8,6 +10,8 @@ public class DelegationService extends
     public void onCreate() {
         super.onCreate();
 
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
         
     }
 }
