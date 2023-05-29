ff6923d6c52db1a0003baab86f89c886bad594f3
Ella Ge <eirage@chromium.org>
2023-05-29 15:02:50 +0900

    Update

--- PWA/Install/manifest.js
+++ PWA/Install/manifest.js
@@ -1,17 +1,17 @@
 var myDynamicManifest = {
     name: "Test App",
     short_name: "Test",
-    start_url: ".",
-    scope: ".",
+    start_url: "https://pwa.eirage.com/Install/index.html",
+    scope: "https://pwa.eirage.com/Install/",
     background_color: "#000000",
     theme_color: "#0f4a73",
     display: "standalone",
     icons: [{
-      "src": "/images/icon-192x192.png",
+      "src": "https:/pwa.eirage.com/images/icon-192x192.png",
       "sizes": "192x192",
       "type": "image/png"
     }, {
-      "src": "/images/icon-512x512.png",
+      "src": "https:/pwa.eirage.com/images/icon-512x512.png",
       "sizes": "512x512",
       "type": "image/png"
     }],
